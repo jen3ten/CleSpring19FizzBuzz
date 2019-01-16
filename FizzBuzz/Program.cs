9cbe55b568857810ca9ab4129e2861859ced5568
Jen E <jen@wecancodeit.org>
2019-01-16 15:03:20 -0500

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is FizzBuzz");
+            Console.WriteLine("Enter a number:");
+            string userEntry = Console.ReadLine();
+            int userEntryAsInt = Convert.ToInt32(userEntry);
+
+            FizzBuzz fizzBuzzer = new FizzBuzz();
+            string fizzBuzzResult = fizzBuzzer.Get(userEntryAsInt);
+            Console.WriteLine(fizzBuzzResult);
+        }
+    }
+}
